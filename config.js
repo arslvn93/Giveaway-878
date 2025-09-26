604c33e5cac7bcc747949e4cdac5d96a1949f895
arslvn93 <58179630+arslvn93@users.noreply.github.com>
2025-09-26 07:05:56 -0400

    Update Site

--- config.js
+++ config.js
@@ -28,7 +28,7 @@ const config = {
     "primaryColorOverride": "",
     "secondaryColorOverride": "",
     "ghlWebhookUrl": "https://services.leadconnectorhq.com/hooks/zjBSfTm1sjZ3gzmZbTG9/webhook-trigger/zNoZImzf9WjxhGdhBKQM",
-    "facebookPixelId": " 1944426839435015",
+    "facebookPixelId": "1312017696453752",
     "salesGeniusAppApi": "",
     "followUpBossEmail": "",
     "webhookUrl": "https://n8n.salesgenius.co/webhook/giveawayupdate"
@@ -180,7 +180,7 @@ const config = {
   },
   "modalQuestions": [
     {
-      "id": "q_auto_1758884699913_0",
+      "id": "q_auto_1758884755800_0",
       "questionText": "What do you like the most about your current home?",
       "options": [
         {
@@ -202,7 +202,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1758884699913_1",
+      "id": "q_auto_1758884755800_1",
       "questionText": "Is a move on your radar in the near future?",
       "options": [
         {
@@ -224,7 +224,7 @@ const config = {
       ]
     },
     {
-      "id": "q_auto_1758884699913_2",
+      "id": "q_auto_1758884755800_2",
       "questionText": "What's your current housing situation?",
       "options": [
         {
